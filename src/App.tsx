35740acf9e5af1813336396777319dad13e9a999
Farid Sattarov <feridsattarov5@gmail.com>
2024-11-10 15:31:20 +0400

    initial submit for trial task

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex flex-row bg-black w-24 h-24'>
+        salam
+      </div>
+    </>
+  )
+}
+
+export default App
