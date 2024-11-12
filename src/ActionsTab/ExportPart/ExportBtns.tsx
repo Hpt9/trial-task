275b7bc804d6e22e276920cc43898bba94d370b3
Farid Sattarov <feridsattarov5@gmail.com>
2024-11-12 17:54:24 +0400

    test

--- src/ActionsTab/ExportPart/ExportBtns.tsx
+++ src/ActionsTab/ExportPart/ExportBtns.tsx
@@ -8,7 +8,7 @@ export default function () {
             <div className="review flex items-center gap-x-2">
                 <Button className="flex items-center gap-x-1 bg-white relative hover:bg-gray-100 p-1.5">
                     <span className="circle flex"></span>
-                    <span>Icmala</span>
+                    <span>Icmal</span>
                 </Button>
                 <Button className="flex items-center gap-x-1 bg-white relative hover:bg-gray-100  p-1.5">
                     <span className="circle flex"></span>
