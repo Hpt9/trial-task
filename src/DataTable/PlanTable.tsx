83864d5b90294c82b236fc04d26ef8bdd7b0aea3
Farid Sattarov <feridsattarov5@gmail.com>
2024-11-13 19:43:06 +0400

    table cahnger added

--- src/DataTable/PlanTable.tsx
+++ src/DataTable/PlanTable.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { AnimatePresence, motion } from "motion/react"
-export default function DetailsTable() {
-    return (
-        <AnimatePresence>
+export default function PlanTable() {
+  return (
+    <AnimatePresence>
         
         <motion.div
             initial={{ x: -10 }}
             animate={{ x: 0 }}
             exit={{ x: -10 }}
-        >DetailsTable</motion.div>
+        >PlansTable</motion.div>
 
         </AnimatePresence>
-    )
+  )
 }
--- src/DataTable/ShowedTable.tsx
+++ src/DataTable/ShowedTable.tsx
@@ -4,12 +4,24 @@ import { data } from '@/data'
 import { ScrollArea,ScrollBar } from "@/components/ui/scroll-area"
 import DetailsTable from './DetailsTable'
 import useViewStore from '@/store/useViewStore'
+import PlanTable from './PlanTable'
+
 
 export default function ShowedTable() {
     const { view } = useViewStore();
+    let TableComponent;
+    if (view === 'progress') {
+        TableComponent = <ProgressTable />;
+    } else if (view === 'details') {
+        TableComponent = <DetailsTable />;
+    } else if (view === 'plan') {
+        TableComponent = <PlanTable />;
+    } else {
+        TableComponent = null; // Handle the case where view doesn't match any option
+    }
   return (
     <ScrollArea className='flex w-full'>
-        {view==="progress"? <ProgressTable/>:<DetailsTable/>}
+        {TableComponent}
         <ScrollBar orientation="horizontal" />
     </ScrollArea>
   )
